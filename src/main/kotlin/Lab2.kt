11ba9ba1e4eb73a6260b3b812fbaa13b9744d899
0JASQUE0 <Ivanbabkinivan3@mail.ru>
2021-11-25 13:42:55 +0300

    fix lab 2

--- src/main/kotlin/Lab2.kt
+++ src/main/kotlin/Lab2.kt
@@ -18,29 +18,36 @@ fun calculateExpression(infixString: String): Double {
     val postfixList = infixToPostfix(infixString)
     val stack = Stack<String>()
     for (item in postfixList) {
-        if (defineType(item) == Type.DIGIT) {
-            stack.push(item)
-        } else if (defineType(item) == Type.CONSTANT) {
-            when (item) {
-                "pi" -> stack.push(Math.PI.toString())
-                "e" -> stack.push(Math.E.toString())
-            }
-        } else if (defineType(item) == Type.FUNCTION) {
-            when (item) {
-                "cos" -> stack.push(cos(stack.pop().toDouble()).toString())
-                "sin" -> stack.push(sin(stack.pop().toDouble()).toString())
-                "tg" -> stack.push(tan(stack.pop().toDouble()).toString())
-                "ctg" -> stack.push((1 / tan(stack.pop().toDouble())).toString())
-                "lg" -> stack.push(log10(stack.pop().toDouble()).toString())
-                "ln" -> stack.push(ln(stack.pop().toDouble()).toString())
-            }
-        } else if (defineType(item) == Type.BINARY_OPERATOR)
-            stack.push(calculate(item, stack.pop().toDouble(), stack.pop().toDouble()).toString())
+        when (defineType(item)) {
+            Type.DIGIT -> stack.push(item)
+            Type.CONSTANT ->
+                when (item) {
+                    "pi" -> stack.push(Math.PI.toString())
+                    "e" -> stack.push(Math.E.toString())
+                }
+            Type.FUNCTION ->
+                when (item) {
+                    "cos" -> stack.push(cos(stack.pop().toDouble()).toString())
+                    "sin" -> stack.push(sin(stack.pop().toDouble()).toString())
+                    "tg" -> stack.push(tan(stack.pop().toDouble()).toString())
+                    "ctg" -> stack.push((1 / tan(stack.pop().toDouble())).toString())
+                    "lg" -> stack.push(log10(stack.pop().toDouble()).toString())
+                    "ln" -> stack.push(ln(stack.pop().toDouble()).toString())
+                }
+            Type.BINARY_OPERATOR -> stack.push(
+                calculate(
+                    item,
+                    stack.pop().toDouble(),
+                    stack.pop().toDouble()
+                ).toString()
+            )
+            else -> throw IllegalArgumentException("incorrect input or something went wrong")
+        }
     }
     return stack.pop().toDouble()
 }
 
-fun infixToPostfix(infixString: String): List<String> {
+private fun infixToPostfix(infixString: String): List<String> {
     val list = stringToList(infixString)
     val postfixList = mutableListOf<String>()
     val stack = Stack<Operator>()
@@ -57,59 +64,54 @@ fun infixToPostfix(infixString: String): List<String> {
             }
             Type.OPEN_BRACKET -> stack.push(item)
             Type.CLOSE_BRACKET -> {
-                if (stack.isEmpty()) throw Exception("incorrect input or something went wrong")
+                if (stack.isEmpty()) throw IllegalArgumentException("incorrect input or something went wrong")
                 var temp: String = stack.pop().operator
                 while (temp != "(") {
-                    if (stack.isEmpty()) throw Exception("incorrect input or something went wrong")
+                    if (stack.isEmpty()) throw IllegalArgumentException("incorrect input or something went wrong")
                     postfixList.add(temp)
                     temp = stack.pop().operator
                 }
             }
-            else -> throw Exception("incorrect input or something went wrong")
+            else -> throw IllegalArgumentException("incorrect input or something went wrong")
         }
     }
     while (stack.isNotEmpty()) {
-        if (stack.last().operator == "(") throw Exception("incorrect input or something went wrong")
+        if (stack.last().operator == "(") throw IllegalArgumentException("incorrect input or something went wrong")
         postfixList.add(stack.pop().operator)
     }
     return postfixList
 }
 
-fun stringToList(infixString: String): List<Operator> {
+private fun stringToList(infixString: String): List<Operator> {
     val list = mutableListOf<Operator>()
     var lastElement = Operator("", -1, Type.NULL)
     val infixStringWithoutSpaces = infixString.replace(" ", "")
-    var flag = false
+    var thereIsUnaryMinus = false
     var tempString = ""
     for (character in infixStringWithoutSpaces) {
-        if (defineType(character.toString()) == Type.DIGIT || defineType(character.toString()) == Type.ERROR || defineType(
-                character.toString()
-            ) == Type.CONSTANT
-        ) {
+        val isItUnaryMinusOrUnaryPlus: Boolean = (character == '-' || character == '+')
+                && (lastElement.type == Type.OPEN_BRACKET || lastElement.type == Type.NULL || lastElement.type == Type.BINARY_OPERATOR) && tempString.isEmpty()
+        val operatorIsStandingIncorrectly: Boolean = (character == '*' || character == '/' || character == '^')
+                && (lastElement.type == Type.OPEN_BRACKET || lastElement.type == Type.NULL || lastElement.type == Type.BINARY_OPERATOR) && tempString.isEmpty()
+        if (symbolIsNumberOrLetter(character)) {
             tempString += character
-        } else if ((character == '-' || character == '+')
-            && (lastElement.type == Type.OPEN_BRACKET || lastElement.type == Type.NULL || lastElement.type == Type.BINARY_OPERATOR) && tempString.isEmpty()
-        ) {
-            // unary '+' and '-'
+        } else if (isItUnaryMinusOrUnaryPlus) {
             if (character == '+') continue
             else {
-                flag = true
+                thereIsUnaryMinus = true
                 continue
             }
-        } else if ((character == '*' || character == '/' || character == '^')
-            && (lastElement.type == Type.OPEN_BRACKET || lastElement.type == Type.NULL || lastElement.type == Type.BINARY_OPERATOR) && tempString.isEmpty()
-        ) {
-            throw Exception("incorrect input or something went wrong")
+        } else if (operatorIsStandingIncorrectly) {
+            throw IllegalArgumentException("incorrect input or something went wrong")
         } else {
             if (tempString.isNotEmpty()) {
-                if (defineType(tempString) == Type.DIGIT)
-                    if (tempString.toDoubleOrNull() == null)
-                        throw Exception("incorrect input or something went wrong")
+                if (defineType(tempString) == Type.ERROR)
+                    throw IllegalArgumentException("incorrect input or something went wrong")
                 lastElement = Operator(tempString, getPriority(tempString), defineType(tempString))
                 tempString = ""
-                if (flag) {
+                if (thereIsUnaryMinus) {
                     lastElement.operator = "-" + lastElement.operator
-                    flag = false
+                    thereIsUnaryMinus = false
                 }
                 list.add(lastElement)
             }
@@ -119,11 +121,10 @@ fun stringToList(infixString: String): List<Operator> {
         }
     }
     if (tempString.isNotEmpty()) {
-        if (defineType(tempString) == Type.DIGIT)
-            if (tempString.toDoubleOrNull() == null)
-                throw Exception("incorrect input or something went wrong")
+        if (defineType(tempString) == Type.ERROR)
+            throw IllegalArgumentException("incorrect input or something went wrong")
         lastElement = Operator(tempString, getPriority(tempString), defineType(tempString))
-        if (flag) {
+        if (thereIsUnaryMinus) {
             lastElement.operator = "-" + lastElement.operator
         }
         list.add(lastElement)
@@ -131,7 +132,7 @@ fun stringToList(infixString: String): List<Operator> {
     return list
 }
 
-fun getPriority(operator: String): Int {
+private fun getPriority(operator: String): Int {
     return when (operator) {
         "cos", "sin", "tg", "ctg", "lg", "ln" -> 4
         "^" -> 3
@@ -142,7 +143,7 @@ fun getPriority(operator: String): Int {
     }
 }
 
-fun defineType(operator: String): Type {
+private fun defineType(operator: String): Type {
     return when (operator) {
         "+", "-", "*", "/", "^" -> Type.BINARY_OPERATOR
         "cos", "sin", "tg", "ctg", "lg", "ln" -> Type.FUNCTION
@@ -156,7 +157,7 @@ fun defineType(operator: String): Type {
     }
 }
 
-fun calculate(
+private fun calculate(
     operator: String,
     secondOperand: Double,
     firstOperand: Double
@@ -169,4 +170,10 @@ fun calculate(
         "^" -> return firstOperand.pow(secondOperand)
     }
     return Double.NaN
+}
+
+private fun symbolIsNumberOrLetter(character: Char): Boolean {
+    return defineType(character.toString()) == Type.DIGIT ||
+            defineType(character.toString()) == Type.ERROR ||
+            defineType(character.toString()) == Type.CONSTANT
 }
